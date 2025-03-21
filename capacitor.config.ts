f7c9d96c0fb6d78e70e89b30ae6951ae12b5c612
tinglypuma4 <esauboycj@gmail.com>
2025-03-21 18:08:48 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Técnicos Via-Digital',
+  webDir: 'dist'
+};
+
+export default config;
